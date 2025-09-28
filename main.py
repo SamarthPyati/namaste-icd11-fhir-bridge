8cdd4226ac9e0b193e7cd06b2738562d0b4d2f87
Samarth Sanjay Pyati <samarthpyati@gmail.com>
2025-09-28 20:14:44 +0530

    Get. Set. Go...

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from namaste-icd11-fhir-bridge!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/play.py
@@ -0,0 +1,8 @@
+import httpx
+
+def main() -> None: 
+    pass 
+
+
+if __name__ == "__main__": 
+    main()
